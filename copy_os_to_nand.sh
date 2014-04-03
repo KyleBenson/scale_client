57c4f0ee23d52c6c0f12d96d0288f9b0f9caffcf
Kyle Benson <kebenson@uci.edu>
2014-04-03 15:51:05 -0700

    fixed bug in copying over filesystem that renamed eth0 -> eth1

--- copy_os_to_nand.sh
+++ copy_os_to_nand.sh
@@ -4,4 +4,8 @@ apt-get install mtd-utils
 ubiformat /dev/mtd2 -s 512; ubiattach /dev/ubi_ctrl -m 2; ubimkvol /dev/ubi0 -N rootfs -m;
 mkfs.ubifs --compr=zlib /dev/ubi0_0; mount -t ubifs ubi0:rootfs /mnt
 mkdir /tmp/rootfs; mount -o bind / /tmp/rootfs/; cp -a /tmp/rootfs/* /mnt/; cp -a /boot /mnt/
+
+# this file holds device-specific information that we want to repopulate on reboot of the new device (ethernet MAC address specifically)
+rm /mnt/etc/udev/rules.d/70-persistent-net.rules 
+
 reboot
