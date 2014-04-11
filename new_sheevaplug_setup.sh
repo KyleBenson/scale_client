42ea669bc3e736e49cd3bcded2784ec4184140e5
Kyle Benson <kyle.edward.benson@gmail.com>
2014-04-10 22:39:08 -0700

    switched setup script to install salt from source

--- new_sheevaplug_setup.sh
+++ new_sheevaplug_setup.sh
@@ -2,9 +2,14 @@
 
 # sets up a newly installed Sheevaplug, including connecting it to the salt-master
 
+MASTER_ADDRESS=SALT_MASTER_IP
+
 # this doesn't work as saltstack.org doesn't have ARMEL support
 #wget --no-check-certificate http://bootstrap.saltstack.org -O install_salt.sh
 #sudo sh install_salt.sh git develop
+
+#TODO: this automatically
+echo 'Make sure we blew away the old file for original plug eth0!'
 
 # Change the hostname to something new, unique, and identifying
 echo "What is the hostname for this device? (Leave blank to keep old one)"
@@ -17,7 +22,22 @@ echo "$REPLY" > /etc/hostname
 sed "s/$OLD_HOSTNAME/$REPLY/g" -i /etc/hosts
 /etc/init.d/hostname.sh start
 
-apt-get install -y python-m2crypto python-pip python-dev 
-#python-zeromq?
-pip install pyzmq PyYAML pycrypto msgpack-python jinja2 psutil
-pip install salt
+# install salt dependencies
+#TODO: psutil, jinja2
+apt-get update
+apt-get install -y python-m2crypto python-pip python-dev git python-sphinx\
+ python-requests python-yaml python-zmq dctrl-tools msgpack-python
+
+# install salt from source using dpkg
+git clone https://github.com/saltstack/salt.git
+cd salt
+debian/rules binary
+cd ..
+dpkg -i salt-common_*.deb salt-minion*.deb
+apt-get -f install
+
+# setup minion
+echo "master: $MASTER_ADDRESS" > /etc/salt/minion
+salt-minion -d
+
+echo "Go to your master and accept the new key from this minion"
