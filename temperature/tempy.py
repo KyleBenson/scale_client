51674fafb33cb241a3b983b8280298387d1f287e
Kyle Benson <kebenson@uci.edu>
2014-04-16 17:50:13 -0700

    added regex and thresholding to basic temperature script

--- temperature/tempy.py
+++ temperature/tempy.py
@@ -1,8 +1,20 @@
-import subprocess
+from __future__ import print_function
+import subprocess, re
 
-cmd = 'examples/temperature-streams'
+cmd = 'temperature-streams'
 result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-#for line in result.stdout:
+
+#NOTE: we assume the temperature is in Celsius and don't scan for the degree symbol
+exp = re.compile(r'Device ([^:]*): Sensor ([0-9]*): Temperature: ([0-9\.]*)')
+
 #NOTE: for line in result.stdout doesn't do "real-time" updates in Py2
 for line in iter(result.stdout.readline, ''):
-    print("%s" % line.strip())
+    match = exp.match(line)
+    try:
+        temperature = float(match.group(3))
+    except AttributeError as e:
+        print('Error parsing temperature: %s' % e)
+        continue
+    print(u'%.2f\N{DEGREE SIGN}C' % temperature)
+    if temperature > 24:
+        print("ALARM: temp > 24!!!")
