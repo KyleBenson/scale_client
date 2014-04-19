1e502bbdd7cc814c7169e321e02b6c087ef15ff7
Charles ZHU <zhuqiuxizju@gmail.com>
2014-04-18 21:25:57 -0700

    Temperature sensor FIXME

--- scale_client/client.py
+++ scale_client/client.py
@@ -10,12 +10,15 @@ from publisher import Publisher
 from mqtt_publisher import MQTTPublisher
 from virtual_sensor import VirtualSensor
 from heartbeat_virtual_sensor import HeartbeatVirtualSensor as HBVirtualSensor
+from usb_virtual_sensor import USBVirtualSensor
+from temperature_virtual_sensor import TemperatureVirtualSensor
 
 QUEUE_SIZE = 4096
 MQTT_HOSTNAME = "m10.cloudmqtt.com"
 MQTT_HOSTPORT = 14664
 MQTT_USERNAME = "mqytlrad"
 MQTT_PASSWORD = "Q_C2GjJvRQrZ"
+CEL_DAEMON_PATH = ""
 
 # Create message queue
 queue = Queue(QUEUE_SIZE)
@@ -43,6 +46,13 @@ ls_vs.append(vs_heartbeat)
 for vs_j in ls_vs:
 	vs_j.daemon = True
 	vs_j.start()
+vs_temperature = TemperatureVirtualSensor(
+	queue,
+	DeviceDescriptor("cel0"),
+	daemon_path = CEL_DAEMON_PATH,
+	threshold = 24.0
+)
+ls_vs.append(vs_temperature)
 
 # Loop forever
 while True:
