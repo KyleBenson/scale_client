a5b12e0a261cfa8789678044bddc587c6c2dcd53
Kyle Benson <kyle.edward.benson@gmail.com>
2014-05-28 19:34:10 -0400

    raspi config script sets hostname, updates right minion file

--- raspi/config_salt.py
+++ raspi/config_salt.py
@@ -1,25 +1,51 @@
 #!/usr/bin/python
 
 # NOTICE
-# 1) Make sure Raspberry Pi is loaded with the latest image, which has a salt installation.
+# 1) Make sure Raspberry Pi is loaded with the latest image,
+#    which has a salt installation.
 # 2) You should be able to write to the configuration directory.
 # 3) Fill in the IP address and port of salt master before running this script.
 
-SALT_MINION_D = "/etc/salt/minion.d"
-SALT_CONFIG_FILE = SALT_MINION_D + "/" + "scale.conf"
-SALT_ID_PREFIX = "raspi"
-SALT_MASTER = "208.76.112.106"
-SALT_MASTER_PORT = 4506
+import subprocess
 
+# remove charles's user info
+subprocess.call('userdel charles; rm -rf /home/charles; rm -rf /home/SmartAmericaSensors', shell=True)
+
+SALT_CONFIG_FILE = "/etc/salt/minion"
+SALT_MASTER = "192.168.1.102 #208.76.112.106"
+SALT_MASTER_PORT = 4506
+SALT_ID_PREFIX = "raspi"
+
+# set hostname using last characters of MAC address as unique ID
 from uuid import getnode
 
-conf = """id: %s%s
+SALT_ID_SUFFIX = ("%012x" % getnode())[6:]
+SALT_HOSTNAME = SALT_ID_PREFIX + SALT_ID_SUFFIX
+
+with open('/etc/hostname') as f:
+    old_hostname = f.read().strip()
+with open('/etc/hostname', 'w') as f:
+    f.write(SALT_HOSTNAME)
+
+# update hosts file
+with open('/etc/hosts') as f:
+    old_hosts = f.read().strip()
+with open('/etc/hosts', 'w') as f:
+    f.write(old_hosts.replace('old_hostname', SALT_HOSTNAME))
+
+# must restart this service to register hostname change
+subprocess.call('/etc/init.d/hostname.sh start', shell=True)
+
+conf = """id: %s
 master: %s
 master_port: %d
-""" % (SALT_ID_PREFIX, ("%012x" % getnode())[6:], SALT_MASTER, SALT_MASTER_PORT)
+""" % (SALT_HOSTNAME, SALT_MASTER, SALT_MASTER_PORT)
 
 print conf
 
 f = open(SALT_CONFIG_FILE, "w")
 f.write(conf)
 f.close()
+
+subprocess.call('rm -rf /etc/salt/minion.d/; rm /etc/salt/pki/minion/minion_master.pub; service salt-minion restart', shell=True)
+print "Go to your master and accept the new key from this minion"
