effc7f0227f2f491adf60bac1a6d226cc38a623e
unknown <zhuqiuxizju@gmail.com>
2014-07-14 13:53:45 -0700

    Done removing 'charles' from all Raspberry Pi

--- raspi/config_salt.py
+++ raspi/config_salt.py
@@ -9,7 +9,7 @@
 import subprocess
 
 # remove charles's user info
-subprocess.call('userdel charles; rm -rf /home/charles; rm -rf /home/SmartAmericaSensors', shell=True)
+#subprocess.call('userdel charles; rm -rf /home/charles; rm -rf /home/SmartAmericaSensors', shell=True)
 
 SALT_CONFIG_FILE = "/etc/salt/minion"
 SALT_MASTER = "128.195.52.93" #"128.195.11.110" #"208.76.112.106"
--- raspi/restore_ACT_LED.sh
+++ raspi/restore_ACT_LED.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
 echo 0 | sudo tee -a /sys/class/leds/led0/brightness
 echo mmc0 | sudo tee -a /sys/class/leds/led0/trigger
