78ea3478c8c396dfd5c91ed35e0754de42c89757
Charles Zhu <zhuqiuxizju+confirm@gmail.com>
2015-01-22 19:21:48 -0800

    Fixed normal setup_pi.sh

--- scale_client/util/setup_pi.sh
+++ scale_client/util/setup_pi.sh
@@ -6,8 +6,10 @@
 #pip install pyserial
 pip install -r requirements.txt
 
+#FIXME: This script is no longer used for setup
 #Make SCALE start when System boots
-cp ../daemons/scale_pi /etc/init.d/scale
+#cp ../daemons/scale_pi /etc/init.d/scale
+#cp scale_pi /etc/init.d/scale
 
 update-rc.d scale defaults
 
