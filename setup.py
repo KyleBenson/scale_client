e8bb42713e08c84b909fd66b4191671f6fcf966c
Charles Zhu <zhuqiuxizju+confirm@gmail.com>
2015-04-03 15:17:02 -0400

    Fixed setup.py for deployment

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ AUTHOR_EMAIL = "kebenson@uci.edu"
 URL = "https://github.com/KyleBenson/SmartAmericaSensors"
 PACKAGES = ["scale_client", "scale_client.core",
             "scale_client.sensors", "scale_client.event_sinks"]
-PACKAGE_DATA = {"scale_client": ["config/default_config.yml"]}
+PACKAGE_DATA = {"scale_client": ["config/*"]}
 DATA_FILES = [("/etc/init.d", ["scripts/scale_daemon"])]
 
 setup(name = NAME,
