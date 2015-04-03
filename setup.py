bc4f141176c71c30d76e13d018e31cbd0ca5f611
Charles Zhu <zhuqiuxizju+confirm@gmail.com>
2015-04-03 15:04:50 -0400

    For deployment (fixed)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name = NAME,
       packages = PACKAGES,
       package_data = PACKAGE_DATA,
       data_files = DATA_FILES,
-      install_requires = ['circuits', 'pyserial', 'mosquitto',
+      requires = ['circuits', 'pyserial', 'mosquitto',
               # TODO: how to only require this on a pi?
               # 'spidev',
               ]
