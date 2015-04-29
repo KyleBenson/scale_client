29a47d0f2d38b7d9cfbc113bb40693545147ad83
Qiuxi Zhu <zhuqiuxizju+confirm@gmail.com>
2015-04-29 11:36:57 -0400

    Moved location sensors to a subfolder

---
+++ scale_client/sensors/dummy_sensors/dummy_temperature_virtual_sensor.py
@@ -0,0 +1,19 @@
+from random import *
+
+from scale_client.sensors.temperature_virtual_sensor import TemperatureVirtualSensor
+
+
+class DummyTemperatureVirtualSensor(TemperatureVirtualSensor):
+    def __init__(self, broker, device=None, threshold=24.0):
+        TemperatureVirtualSensor.__init__(self, broker, device=device, threshold=threshold)
+        self._rand = Random()
+        self._rand.seed()
+        self._darkflag = True
+        self.CEL_MEAN = threshold - 2
+
+    def read_raw(self):
+        return round(self.CEL_MEAN + self._rand.random() * 6 - 3, 2)
+
+    def on_start(self):
+        # avoid opening any connections to real sensors, so skip the on_start() of our parent
+        super(TemperatureVirtualSensor, self).on_start()
