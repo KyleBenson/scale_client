6760318bad5f7807f652b5ed71f5c0d28a55445e
Qiuxi Zhu <zhuqiuxizju+confirm@gmail.com>
2015-04-29 12:19:51 -0400

    Support of alt in event reporter

--- scale_client/core/event_reporter.py
+++ scale_client/core/event_reporter.py
@@ -58,6 +58,8 @@ class EventReporter(Application):
         elif type(self._location) == type({}):
             event.data["geotag"] = {"lon": self._location["lon"],
                                     "lat": self._location["lat"]}
+            if "alt" in self._location:
+            	event.data["geotag"]["alt"] = self._location["alt"]
         else: raise ValueError
 
         # Send event to sinks
