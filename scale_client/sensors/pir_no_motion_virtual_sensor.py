9ec2763190cc54b757044dbd0108453c3b9340ea
Qiuxi Zhu <zhuqiuxizju+confirm@gmail.com>
2015-04-30 13:52:22 -0400

    New implementation for no motion VS, not tested

--- scale_client/sensors/pir_no_motion_virtual_sensor.py
+++ scale_client/sensors/pir_no_motion_virtual_sensor.py
@@ -7,15 +7,30 @@ log = logging.getLogger(__name__)
 
 
 class PIRNoMotionVirtualSensor(VirtualSensor):
-    def __init__(self, broker, device=None, inact_threshold=600):
-        super(PIRNoMotionVirtualSensor, self).__init__(broker=broker, device=device, interval=None)
-        self._inact_timer = None
+    def __init__(self, broker, device=None, interval=1, inact_threshold=600):
+        super(PIRNoMotionVirtualSensor, self).__init__(broker=broker, device=device, interval=interval)
+        self._inact_timer = get_time()
         self._inact_threshold = inact_threshold
 
     DEFAULT_PRIORITY = 7
 
     def get_type(self):
         return "no_motion"
+
+    def read_raw(self):
+    	return PIRVirtualSensor.IDLE
+
+    def read(self):
+    	event = super(PIRNoMotionVirtualSensor, self).read()
+    	event.data["condition"] = self.__get_condition()
+        return event
+
+    def __get_condition(self):
+    	return{"inactive_time": {
+                    "operator": ">",
+                    "value": self._inact_threshold
+                }
+            }
 
     def on_event(self, event, topic):
         et = event.get_type()
@@ -25,20 +40,13 @@ class PIRNoMotionVirtualSensor(VirtualSensor):
             return
 
         if ed == PIRVirtualSensor.IDLE:
-            if self._inact_timer is None:
-                self._inact_timer = get_time()
-            elif self._inact_timer + self.inact_threshold < get_time():
-                new_event = self.make_event_with_raw_data(ed, priority=self.__class__.DEFAULT_PRIORITY)
-                new_event.data["condition"] = {
-                        "inactive_time": {
-                            "operator": ">",
-                            "value": self._inact_threshold
-                        }
-                    }
-                self.publish(new_event)
-                self._inact_timer = get_time()
-        elif ed == PIRVirtualSensor.ACTIVE:
+            self._inact_timer = get_time()
+        else:
             self._inact_timer = None
 
     def policy_check(self, event):
+    	if self._inact_timer is not None:
+    		if self._inact_timer + self.inact_threshold < get_time():
+    			self._inact_timer = get_time()
+    			return True
         return False
