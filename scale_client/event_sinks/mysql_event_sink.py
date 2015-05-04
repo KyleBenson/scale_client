24b4ffee1340554631b024927102708848f67451
Charles Zhu <zhuqiuxizju+confirm@gmail.com>
2015-05-04 10:16:03 -0700

    MySQL event sink

---
+++ scale_client/event_sinks/mysql_event_sink.py
@@ -0,0 +1,100 @@
+import json
+import sys
+import peewee
+
+from scale_client.event_sinks.event_sink import EventSink
+
+import logging
+log = logging.getLogger(__name__)
+
+class MySQLEventSink(EventSink):
+	def __init__(self, broker, dbname, username, password):
+		super(MySQLEventSink, self).__init__(broker)
+
+		self._dbname = dbname
+		self._username = username
+		self._password = password
+
+		self._db = None
+
+	class EventRecord(peewee.Model):
+		class Meta:
+			database = None
+			db_table = "events"
+
+		event = peewee.CharField(max_length = 32)
+		priority = peewee.IntegerField()
+		timestamp = peewee.DoubleField()
+		geotag = peewee.TextField(null = True)
+		value_json = peewee.TextField()
+		upload_time = peewee.DoubleField(null = True)
+
+	def _try_connect(self):
+		self._db = peewee.MySQLDatabase(
+				self._dbname,
+				user=self._username,
+				passwd=self._password
+			)
+		try:
+			self._db.connect()
+		except peewee.OperationalError, err:
+			self._db = None
+			log.error(str(err))
+		self.EventRecord._meta.database = self._db
+		if self._db is None:
+			return False
+		try:
+			self.EventRecord.create_table()
+			log.info("Created table: "+ str(self.EventRecord._meta.db_table))
+		except peewee.OperationalError:
+			pass
+		return True
+
+	def on_start(self):
+		self._try_connect()
+
+	def encode_event(self, event):
+		# If event is from database, update upload stamp
+		if hasattr(event, "db_record"):
+			table_id = event.db_record["table_id"]
+			upload_time = event.db_record["upload_time"]
+			encoded_event = self.EventRecord.update(
+					upload_time = upload_time
+				).where(self.EventRecord.id == table_id)
+			return encoded_event
+
+		# If event is NOT from database, insert into database
+		geotag = None
+		if "geotag" in event.data:
+			geotag = event.data["geotag"]
+		encoded_event = self.EventRecord(
+				event=event.data["event"],
+				priority=event.priority,
+				timestamp=event.timestamp,
+				geotag=geotag,
+				value_json=json.dumps(event.data["value"])
+			)
+		return encoded_event
+	
+	def check_available(self, event):
+		if self._db is None:
+			#log.debug("not available")
+			return self._try_connect()
+		#log.debug("available")
+		return True
+
+	def send(self, encoded_event):
+		try:
+			if isinstance(encoded_event, self.EventRecord):
+				encoded_event.save()
+				log.info("MySQL record saved to " + str(self.EventRecord._meta.db_table))
+				return True
+			elif isinstance(encoded_event, peewee.UpdateQuery):
+				encoded_event.execute()
+				log.debug("MySQL record updated")
+				return True
+		except peewee.OperationalError, err:
+			log.error(str(err))
+			self._db = None
+		return False
+
