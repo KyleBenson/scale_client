0b89d0e836277682c00f915756ec7b721cdc9dd8
Charles Zhu <zhuqiuxizju+confirm@gmail.com>
2015-05-07 10:38:52 -0400

    N5 resistor virtual sensor tested

--- scale_client/sensors/n5_resistor_virtual_sensor.py
+++ scale_client/sensors/n5_resistor_virtual_sensor.py
@@ -7,6 +7,12 @@ class N5ResistorVirtualSensor(AnalogVirtualSensor):
 	def __init__(self, broker, device=None, interval=1, analog_port=None):
 		super(N5ResistorVirtualSensor, self).__init__(broker, device, interval, analog_port)
 
+		# Check if configurations in super class are correct
+		if self.HDWR_MAX is None or self.HDWR_MAX <= 0:
+			raise ValueError
+		if self.READ_MAX is not None and self.READ_MAX <= 0.0:
+			raise ValueError
+
 	DEFAULT_PRIORITY = 9
 
 	VOLT_ADC = 5.0
@@ -18,11 +24,15 @@ class N5ResistorVirtualSensor(AnalogVirtualSensor):
 		adcout = super(N5ResistorVirtualSensor, self).read_raw()
 		adcmax = self.READ_MAX or self.HDWR_MAX
 		volt_x = 1.0 * self.VOLT_ADC * adcout / adcmax
+		if volt_x == 0.0: #XXX
+			return None
 		rsst_s = (1.0 * self.VOLT_ALL / volt_x - 1) * self.RSST_STD
-		return rsst_s / self.RSST_AIR
+		return round(rsst_s / self.RSST_AIR, 3)
 
 	def get_type(self):
-        return "raw_n5_resistor"
+		return "raw_n5_resistor"
 
-    def policy_check(self, data):
-        return True
+	def policy_check(self, data):
+		if data.get_raw_data() is None:
+			return False
+		return True
