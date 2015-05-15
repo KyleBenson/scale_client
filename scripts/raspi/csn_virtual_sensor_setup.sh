07629c65443635451032b47a0b303ff997619ae1
root <root@raspberrypi.(none)>
2015-05-15 01:41:50 +0000

    added working csn_virtual_sensor setup script and csnd ini script

--- scripts/raspi/csn_virtual_sensor_setup.sh
+++ scripts/raspi/csn_virtual_sensor_setup.sh
@@ -12,13 +12,11 @@ sudo apt-get install libssl-dev -y
 pip install WebOb
 pip install Paste
 pip install webapp2
-apt-get install python-protobuf
+apt-get install python-protobuf -y
 
-ln -s ../../CsnBin/csnd /usr/bins/ 
-ln -s ../../CsnBin/csn-config /usr/bins/ 
+cp ../../csn_bin/csnd /usr/sbin/ 
+cp ../../csn_bin/csn-config /usr/sbin/ 
 
 cp csnd /etc/init.d/
 update-rc.d csnd defaults
 /etc/init.d/csnd start
-
-echo "127.0.0.1 csn-server.appspot.com" >> /etc/hosts 
