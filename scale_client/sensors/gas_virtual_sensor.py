3ff5cc4ab59a2775af913b98290f693dce6fd884
Charles Zhu <zhuqiuxizju+confirm@gmail.com>
2015-05-27 18:48:05 -0400

    Gas sensor supports customized gas type

--- scale_client/sensors/gas_virtual_sensor.py
+++ scale_client/sensors/gas_virtual_sensor.py
@@ -2,13 +2,16 @@ from scale_client.sensors.analog_virtual_sensor import AnalogVirtualSensor
 
 
 class GasVirtualSensor(AnalogVirtualSensor):
-    def __init__(self, broker, device=None, interval=1, analog_port=None, threshold=400):
+    def __init__(self, broker, device=None, interval=1, analog_port=None, threshold=400, gas_type=None):
         super(GasVirtualSensor, self).__init__(broker, device=device, interval=interval, analog_port=analog_port)
         self._threshold = threshold
+        self._gas_type = gas_type
 
     DEFAULT_PRIORITY = 3
 
     def get_type(self):
+        if type(self._gas_type) == type(""):
+            return self._gas_type
         return "explosive_gas"
 
     def read(self):
