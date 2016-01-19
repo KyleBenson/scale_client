186f2c33db60025fd4173da0ea23b3bc2f7af4b1
Andrew Yang <a.yang629@Andrews-MacBook-Pro.local>
2016-01-18 16:01:21 -0800

    Checking GeoCRON dummy

---
+++ scale_client/event_sinks/geocron_event_sink.py
@@ -0,0 +1,20 @@
+from scale_client.event_sinks.event_sink import EventSink
+
+import json
+import logging
+log = logging.getLogger(__name__)
+# go ahead and set the logger to INFO here so that we always log the events in question
+log.setLevel(logging.INFO)
+
+
+class GeocronEventSink(EventSink):
+    """
+    This EventSink simply prints all sunk SensedEvents to log.info
+    """
+
+    def send(self, encoded_event):
+        msg = "geocron event sunk: %s" % encoded_event
+        log.info(msg)
+
+    def encode_event(self, event):
+    	return json.dumps(event.to_map())
