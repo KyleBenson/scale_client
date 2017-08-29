70fd805fb55ee6b364522bb268da02ce627cd9e3
Kyle Benson <kyle.edward.benson@gmail.com>
2017-08-28 17:15:01 -0700

    fixed TestApp to create a new event instance before modifying and publishing it

--- scale_client/applications/test_application.py
+++ scale_client/applications/test_application.py
@@ -1,7 +1,11 @@
+from copy import deepcopy
+
 from scale_client.core.application import Application
 import logging
 log = logging.getLogger(__name__)
 
+# type annotations
+from scale_client.core.sensed_event import SensedEvent
 
 class TestApplication(Application):
     """
@@ -24,31 +28,37 @@ class TestApplication(Application):
         This callback is where you should handle the Application's logic for responding to events to which it has
         subscribed.
         :param event: the Event just published
+        :type event: SensedEvent
         :param topic: the Topic the Event was published to
         """
         if topic is None:
             topic = event.topic
 
+        # To make sure we don't loop forever (modifying this event each time), we only touch it once
         if not event.metadata.has_key('touched_by_test_app'):
             log.debug("TestApp received event (topic %s): %s" % (topic, event))
 
-        raw_data = event.data
-        try:
-            event.data = raw_data + self.increment
-        except (ValueError, TypeError):
-            # maybe not be a numeric value?
+            # Now we try to transform the event's data by first treating it as a numeric...
+            raw_data = event.data
             try:
-                event.data = float(raw_data) + self.increment
-            except ValueError:
-                # can't even parse a numeric, so hopefully a string?
-                if isinstance(raw_data, str):
-                    event.data = raw_data + str(self.increment)
-                else:
-                    event.data = self.increment
-                    event.event_type = "TEST_EVENT"
+                new_data = raw_data + self.increment
+            except (ValueError, TypeError):
+                # maybe not be a numeric value?
+                try:
+                    new_data = float(raw_data) + self.increment
+                except ValueError:
+                    # can't even parse a numeric, so hopefully a string?
+                    if isinstance(raw_data, str):
+                        new_data = raw_data + str(self.increment)
+                    else:
+                        new_data = self.increment
 
-        # TODO: perhaps the topic should be different to avoid loops?  For now, we just
-        # XXX: hack to avoid infinite looping with this event
-        if not event.metadata.has_key('touched_by_test_app'):
+            # We create a new event from this one and then publish it!
+            condition = {'event': event}
+            event = deepcopy(event)
+            event.data = new_data
+            # especially important if the event is a remote one!
+            event.source = self.path
+            event.condition = condition
             event.metadata['touched_by_test_app'] = True
             self.publish(event, topic)
