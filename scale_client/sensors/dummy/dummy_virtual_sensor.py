14ff150a477748f718035402e203f65fbe44b05a
Kyle Benson <kyle.edward.benson@gmail.com>
2017-09-11 16:41:14 -0700

    added ability for DummyVS to write in an output file each SensedEvent it creates

--- scale_client/sensors/dummy/dummy_virtual_sensor.py
+++ scale_client/sensors/dummy/dummy_virtual_sensor.py
@@ -1,3 +1,4 @@
+import json
 import logging
 log = logging.getLogger(__name__)
 
@@ -10,11 +11,16 @@ class DummyVirtualSensor(VirtualSensor):
     Future versions will support dynamically-generated SensedEvent data and a dynamic sample_interval.
     """
 
-    def __init__(self, broker, start_delay=0, sample_interval=1, static_event_data="dummy_data", dynamic_event_data=None, **kwargs):
+    def __init__(self, broker, sample_interval=1,
+                 # new parameters
+                 start_delay=0, output_events_file=None,
+                 static_event_data="dummy_data", dynamic_event_data=None, **kwargs):
         """
         :param broker:
+        :param sample_interval: set to 1 second by default for generating basic dummy data; set to None to disable synchronous mode
         :param start_delay: on_start will be delayed by this many seconds
-        :param sample_interval: set to 1 second by default for generating basic dummy data
+        :param output_events_file: when specified, save each created SensedEvent to the requested file,
+        which will be a JSON-encoded list of events
         :param static_event_data:
         :param dynamic_event_data:
         :param kwargs:
@@ -27,9 +33,29 @@ class DummyVirtualSensor(VirtualSensor):
         self.start_delay = start_delay
         self.static_event_data = static_event_data
 
+        # If we enabled event logging to file, start a list we'll append each new one in
+        self.output_events_file = output_events_file
+        if self.output_events_file is not None:
+            self.__output_events = []
+        else:
+            self.__output_events = None
+
     def read_raw(self):
         # TODO: handle dynamic_event_data
         return self.static_event_data
+
+    def read(self):
+        event = super(DummyVirtualSensor, self).read()
+        if self.__output_events is not None:
+            self.__output_events.append(event)
+        return event
+
+    def on_stop(self):
+        super(DummyVirtualSensor, self).on_stop()
+        if self.output_events_file:
+            with open(self.output_events_file, 'w') as f:
+                # since we don't have an actual SensedEvent encoder, just convert each to a map first
+                f.write(json.dumps([e.to_map() for e in self.__output_events]))
 
     def on_start(self):
         """
