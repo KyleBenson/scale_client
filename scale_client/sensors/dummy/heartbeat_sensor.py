dd52b593ae0760d0507576b229cff3921ff0003e
Kyle Benson <kyle.edward.benson@gmail.com>
2017-10-08 00:48:13 -0700

    typo in event_type

--- scale_client/sensors/dummy/heartbeat_sensor.py
+++ scale_client/sensors/dummy/heartbeat_sensor.py
@@ -10,7 +10,7 @@ class HeartbeatSensor(DummyVirtualSensor):
     useful for ensuring a remote client is alive and connected to the data exchange.
     """
 
-    def __init__(self, broker, static_event_data="heartbeat", event_type="hearbeat", sample_interval=5, **kwargs):
+    def __init__(self, broker, static_event_data="heartbeat", event_type="heartbeat", sample_interval=5, **kwargs):
         super(HeartbeatSensor, self).__init__(broker=broker, static_event_data=static_event_data,
                                               event_type=event_type, sample_interval=sample_interval, **kwargs)
 
