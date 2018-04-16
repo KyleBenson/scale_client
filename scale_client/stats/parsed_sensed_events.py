a56b9093f5b209ba32581441e9de7eb34d8a42d0
Kyle Benson <kyle.edward.benson@gmail.com>
2018-04-16 13:51:05 -0700

    added helper function for combining columns with params (derived class can override now); timezone fix for single time entry

--- scale_client/stats/parsed_sensed_events.py
+++ scale_client/stats/parsed_sensed_events.py
@@ -18,7 +18,7 @@ class ParsedSensedEvents(pd.DataFrame):
     they're in Unix epoch format!
     """
 
-    def __init__(self, data, timezone=DEFAULT_TIMEZONE, **kwargs):
+    def __init__(self, data, filename=None, timezone=DEFAULT_TIMEZONE, **kwargs):
         """
         Parses the given data into a dict of events recorded by the client
          and passes the resulting data into a pandas.DataFrame with additional columns specified by kwargs,
@@ -26,6 +26,7 @@ class ParsedSensedEvents(pd.DataFrame):
 
         :param data: raw string containing JSON object-like data e.g. nested dicts/lists
         :type data: str
+        :param filename: name of the file from which this data is being parsed (not used by default)
         :param timezone: the timezone to use for converting time columns (default='America/Los_Angeles'); set to None to disable conversion
         :param kwargs: additional static values for columns to distinguish this group of events from others
             e.g. host_id, host_ip
@@ -36,11 +37,17 @@ class ParsedSensedEvents(pd.DataFrame):
 
         # sub-classes may extract the column data in different ways depending on the output format
         columns = self.extract_columns(data)
-        columns.update(kwargs)
+
+        # similarly, sub-classes may only wish to add certain parameters from kwargs
+        columns = self.combine_params_columns(columns, **kwargs)
 
         self.convert_columns(columns, timezone=timezone)
 
-        super(ParsedSensedEvents, self).__init__(columns)
+        try:
+            super(ParsedSensedEvents, self).__init__(columns)
+        except BaseException as e:
+            log.error("failed to build DataFrame with columns: %s" % columns)
+            raise
 
     def parse_data(self, data, **params):
         """Override this to parse the raw data string using a format other than JSON.  params is ignored by default,
@@ -78,6 +85,19 @@ class ParsedSensedEvents(pd.DataFrame):
 
         return cols
 
+    def combine_params_columns(self, columns, **params):
+        """
+        Adds the relevant parameters to the given columns.  By default, simply adds ALL params to columns.
+        :param columns:
+        :type columns: dict
+        :param params:
+        :return:
+        :rtype: dict
+        """
+
+        columns.update(params)
+        return columns
+
     def convert_columns(self, columns, timezone=None):
         """
         Converts the columns to more specific pandas data types: this is where we convert time columns.
@@ -88,7 +108,15 @@ class ParsedSensedEvents(pd.DataFrame):
             # XXX: any columns starting with 'time' should be converted to pandas.Timestamp!
             if k.startswith('time') and timezone:
                 columns[k] = pd.to_datetime(val, unit='s')
-                columns[k].tz = timezone
+
+                # Now set the timezone:
+                # XXX: for scalar times (e.g. sim start time) we need to convert the datetime's timezone via Python:
+                if not isinstance(columns[k], pd.DatetimeIndex):
+                    import pytz
+                    columns[k] = columns[k].replace(tzinfo=pytz.timezone(timezone))
+                else:
+                    columns[k].tz = timezone
+
             # TODO: figure out how to take a single value and share it through all rows as a category when it's just a string.
             # BUT: we can't have the values of a category be null so how to do this with messages that never arrive?
             # XXX: basically everything else that isn't numeric is a category
